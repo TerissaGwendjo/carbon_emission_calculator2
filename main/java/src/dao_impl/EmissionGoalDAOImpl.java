55ff565941e2995bbb701b4a15cc41ca842571ed
dci-student <Terissa>
2024-04-17 10:02:50 +0200

    carbon copy emission 2

--- main/java/src/dao_impl/EmissionGoalDAOImpl.java
+++ main/java/src/dao_impl/EmissionGoalDAOImpl.java
@@ -2,6 +2,7 @@ package java.src.dao_impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.src.EmissionGoal;
 import java.src.dao_interfaces.EmissionGoalDAO;
@@ -30,16 +31,58 @@ public class EmissionGoalDAOImpl implements EmissionGoalDAO {
 
     @Override
     public EmissionGoal getEmissionGoal(Long goalId) {
+        String sql = EmissionGoalCRUDQueries.GET_GOAL.getQuery();
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, goalId);
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return extractEmissionGoalFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void updateEmissionGoal(EmissionGoal emissionGoal) {
 
+        String sql = EmissionGoalCRUDQueries.UPDATE_GOAL.getQuery();
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, emissionGoal.getUserId());
+            pstmt.setDouble(2, emissionGoal.getTargetEmission());
+            pstmt.setDate(3, java.sql.Date.valueOf(emissionGoal.getStartDate()));
+            pstmt.setDate(4, java.sql.Date.valueOf(emissionGoal.getEndDate()));
+            pstmt.setString(5, emissionGoal.getStatus());
+            pstmt.setLong(6, emissionGoal.getGoalId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void deleteEmissionGoal(Long goalId) {
+        String sql = EmissionGoalCRUDQueries.DELETE_GOAL.getQuery();
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setLong(1, goalId);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    private EmissionGoal extractEmissionGoalFromResultSet(ResultSet resultSet) throws SQLException {
+        EmissionGoal emissionGoal = new EmissionGoal();
+        emissionGoal.setGoalId(resultSet.getLong("goal_id"));
+        emissionGoal.setUserId(resultSet.getLong("user_id"));
+        emissionGoal.setTargetEmission(resultSet.getDouble("target_emission"));
+        emissionGoal.setStartDate(resultSet.getDate("start_date").toLocalDate());
+        emissionGoal.setEndDate(resultSet.getDate("end_date").toLocalDate());
+        emissionGoal.setStatus(resultSet.getString("status"));
+
+        return emissionGoal;
+    }
+
 }
--- main/java/src/model_classes/Activity.java
+++ main/java/src/model_classes/Activity.java
@@ -18,6 +18,32 @@ public class Activity {
         this.name = name;
         this.description = description;
     }
+
+    // Getters and setters
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
         return "Activity{" +
